dc27bf8bc15c0770d8d6a1e4bc40268b47caa6a2
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-14 18:51:59 -0500

    boards info showing

---
+++ src/javascripts/components/boards/boards.js
@@ -0,0 +1,12 @@
+import boardData from '../../helpers/data/boardsData';
+
+const initBoards = () => {
+  boardData.loadBoards()
+    .then((resp) => {
+      console.error('resp', resp.data.boards);
+    })
+    .catch(err => console.error('error from loadBoards', err));
+};
+
+
+export default { initBoards };

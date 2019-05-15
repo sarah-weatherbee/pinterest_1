68ef35ae5a608267d0752d4f775d1d35eb34fe47
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-14 19:24:54 -0500

    boards are printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,12 +1,24 @@
 import boardData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+// 7:13 make domstring to display boards
+const domStringBuilder = (boards) => {
+  let domString = '';
+  boards.forEach((board) => {
+    domString += `<div id=${board.id} class="card board col-3">`;
+    domString += `<h3 class="card-text"> ${board.name}"</h3>`;
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      // console.error('resp', resp.data.boards);
+      // the spot where we know we have data back from the promise
+      domStringBuilder(resp.data.boards);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
-
-
 export default { initBoards };

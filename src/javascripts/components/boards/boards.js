bf418c7d46067acf0b73fba62e8a35843e0e5130
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-14 19:39:13 -0500

    board buttons work

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,15 +1,36 @@
 import boardData from '../../helpers/data/boardsData';
 import util from '../../helpers/util';
 
+// 7:30
+const seePinDiv = (e) => {
+  // 7:36
+  const boardId = e.target.closest('.card').id;
+  console.error('you clicked a button', boardId);
+  // 7:33
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.remove('hide');
+};
+
+// 7:28
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
+
 // 7:13 make domstring to display boards
 const domStringBuilder = (boards) => {
   let domString = '';
   boards.forEach((board) => {
     domString += `<div id=${board.id} class="card board col-3">`;
     domString += `<h3 class="card-text"> ${board.name}"</h3>`;
+    // 7:27 make button to display num of pins and take user to pins
+    domString += '<button class="btn btn-warning see-pins">Pins</button>';
     domString += '</div>';
   });
   util.printToDom('user-boards', domString);
+  bindEvents();
 };
 
 const initBoards = () => {
